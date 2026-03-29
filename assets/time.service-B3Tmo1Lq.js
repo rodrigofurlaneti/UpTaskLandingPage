1e482b5048b07235ac72d236318e8d2de368cbb7
Rodrigo Luiz Madeira Furlaneti <rodrigofurlaneti31@hotmail.com>
2026-03-29 12:44:50 -0300

    Added

---
+++ assets/time.service-B3Tmo1Lq.js
@@ -0,0 +1,1 @@
+import{N as a}from"./index-BmsmA8Hc.js";const m={log:t=>a.post("/time",t).then(e=>e.data),getByTask:t=>a.get(`/time/task/${t}`).then(e=>e.data),delete:t=>a.delete(`/time/${t}`)};export{m as t};
