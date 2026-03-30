07a760c084bbc0bdec07e338cf005da27c012f82
Rodrigo Luiz Madeira Furlaneti <rodrigofurlaneti31@hotmail.com>
2026-03-29 22:10:47 -0300

    Added

---
+++ assets/js/companies.service-Dh7elUyi.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index-NsA5tNdI.js";const t={getMembers:()=>e.get("/companies/members").then(e=>e.data),getSubscription:()=>e.get("/companies/subscription").then(e=>e.data)};export{t as c};
