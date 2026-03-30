ceef4f78a0dfb14a8f66cf9894d7101c6b67f482
Rodrigo Luiz Madeira Furlaneti <rodrigofurlaneti31@hotmail.com>
2026-03-30 09:08:20 -0300

    Added

---
+++ assets/js/companies.service-Dz3penxw.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index-yoIaOJim.js";const t={getMembers:()=>e.get("/companies/members").then(e=>e.data),getSubscription:()=>e.get("/companies/subscription").then(e=>e.data)};export{t as c};
---
+++ assets/js/time.service-B6GUkAVs.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index-yoIaOJim.js";const e={log:e=>t.post("/time",e).then(t=>t.data),getByTask:e=>t.get(`/time/task/${e}`).then(t=>t.data),delete:e=>t.delete(`/time/${e}`)};export{e as t};
